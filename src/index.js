9dee6390195d55614638c6f3a14ee8638d006e3d
David Harrigan <dave@Davids-MacBook-Pro.local>
2018-12-23 23:42:10 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+require('normalize.css/normalize.css');
+require('./styles/index.scss');
+
+import PopperJs from 'popper.js';
+import jquery from 'jquery';
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const pluginsTriggerElement = document.getElementById('plugins-trigger');
+    const pluginsElement = document.getElementById('plugins');
+
+    const pluginsVisibleClass = "splash-overview-plugins__list--visible";
+
+    pluginsTriggerElement.onclick = () => {
+        pluginsElement.classList.toggle(pluginsVisibleClass);
+    }
+
+    /***************** Smooth Scrolling ******************/
+
+    $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 90
+                    }, 2000);
+                    return false;
+                }
+            }
+        });
+    });
+});
